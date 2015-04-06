103e2fcbdd751c62cb2564380b5a707ec3dd6a6d
Luke Carrier <luke@carrier.im>
2015-04-06 12:14:14 +0100

    WinRM works now
    Apparently this is normal behaviour!?
    
    http://stackoverflow.com/questions/11638705/why-does-calling-a-nested-
    batch-file-without-prepending-call-to-the-line-exit

--- scripts/winrm.bat
+++ scripts/winrm.bat
@@ -8,9 +8,17 @@ REM @copyright 2015 Luke Carrier
 REM @license GPL v3
 REM
 
-winrm quickconfig -q
-winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
-winrm set winrm/config @{MaxTimeoutms="1800000"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/service/auth @{Basic="true"}
+call winrm quickconfig -q
+call winrm quickconfig -transport:http
+
+call winrm set winrm/config @{MaxTimeoutms="1800000"}
+call winrm set winrm/config/service @{AllowUnencrypted="true"}
+call winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
+call winrm set winrm/config/client/auth @{Basic="true"}
+call winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+call winrm set winrm/config/service/auth @{Basic="true"}
+
 sc config WinRM start= auto
+
+timeout 5
+sc query winrm
