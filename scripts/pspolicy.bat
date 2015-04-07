9604790a63e7688e0a2b966eaa530ceb1be64e70
Luke Carrier <luke@carrier.im>
2015-04-07 01:57:35 +0100

    So the UAC script sorta didn't do as labelled
    * Set PowerShell execution policy
    * Disable UAC
    * Reorder scripts for tidiness

--- scripts/pspolicy.bat
+++ scripts/pspolicy.bat
@@ -8,4 +8,4 @@ REM @copyright 2015 Luke Carrier
 REM @license GPL v3
 REM
 
-cmd /c reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+cmd /c reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /t REG_SZ /d RemoteSigned /f
