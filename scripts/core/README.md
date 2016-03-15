a6eed7f729a095b06dbf792141b43d59d4201355
Luke Carrier <luke@carrier.im>
2016-03-15 17:11:43 +0000

    Relocate core scripts

---
+++ scripts/core/README.md
@@ -0,0 +1,7 @@
+# Core installation scripts
+
+These base installation scripts are responsible for configuring security policy,
+disabling automatic updates to prevent unexpected reboots and enabling remote
+management of the built VMs.
+
+* * *
--- scripts/core/winrm.bat
+++ scripts/core/winrm.bat
@@ -13,7 +13,7 @@ call winrm quickconfig -transport:http
 
 call winrm set winrm/config @{MaxTimeoutms="1800000"}
 call winrm set winrm/config/service @{AllowUnencrypted="true"}
-call winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
+call winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
 call winrm set winrm/config/client/auth @{Basic="true"}
 call winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 call winrm set winrm/config/service/auth @{Basic="true"}
