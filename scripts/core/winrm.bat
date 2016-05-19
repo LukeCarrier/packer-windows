bcbc8548ab9094884bf199a8b7604eedb3a23102
Luke Carrier <luke@carrier.im>
2016-05-19 11:07:32 +0100

    call => CALL

--- scripts/core/winrm.bat
+++ scripts/core/winrm.bat
@@ -1,13 +1,13 @@
 @ECHO off
 
-call winrm quickconfig -q
-call winrm quickconfig -transport:http
+CALL winrm quickconfig -q
+CALL winrm quickconfig -transport:http
 
-call winrm set winrm/config @{MaxTimeoutms="1800000"}
-call winrm set winrm/config/service @{AllowUnencrypted="true"}
-call winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
-call winrm set winrm/config/client/auth @{Basic="true"}
-call winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
-call winrm set winrm/config/service/auth @{Basic="true"}
+CALL winrm set winrm/config @{MaxTimeoutms="1800000"}
+CALL winrm set winrm/config/service @{AllowUnencrypted="true"}
+CALL winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
+CALL winrm set winrm/config/client/auth @{Basic="true"}
+CALL winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+CALL winrm set winrm/config/service/auth @{Basic="true"}
 
 sc config WinRM start= auto
