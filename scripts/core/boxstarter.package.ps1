fd17c35c08a2f419ff89e893220c7f3a1eab8aa9
Luke Carrier <luke@carrier.im>
2016-12-18 03:30:42 +0000

    Clean up WinRM configuration

--- scripts/core/boxstarter.package.ps1
+++ scripts/core/boxstarter.package.ps1
@@ -88,11 +88,14 @@ if (Test-StageOutstanding "ExecuteSysprep") {
 if (Test-StageOutstanding "EnableWinRM") {
     Write-BoxstarterMessage "Enabling WinRM"
     Enable-PSRemoting -Force
-    & winrm set winrm/config "@{MaxTimeoutms=`"1800000`"}"
-    & winrm set winrm/config/service "@{AllowUnencrypted=`"true`"}"
-    & winrm set winrm/config/winrs "@{MaxMemoryPerShellMB=`"2048`"}"
-    & winrm set winrm/config/client/auth "@{Basic=`"true`"}"
-    & winrm set winrm/config/listener?Address=*+Transport=HTTP "@{Port=`"5985`"}"
-    & winrm set winrm/config/service/auth "@{Basic=`"true`"}"
+
+    Set-Item -Path WSMan:\localhost\MaxTimeoutms             -Value 1800000
+    Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value True
+    Set-Item -Path WSMan:\localhost\Service\Auth\Basic       -Value True
+
+    Set-Item -Path WSMan:\localhost\Shell\MaxMemoryPerShellMB -Value 2048
+
+    Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
+
     Write-StageComplete "EnableWinRM"
 }
